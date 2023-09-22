2dc5d8cc4c68c372fa0254228bf5417caa6c9044
tanhoang088 <tanhoang088@gmail.com>
2023-09-22 17:14:34 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:calendar_flutter/page/createEvent_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,20 +31,116 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  bool isCalendar = false;
+  bool isTodayjob = false;
+  bool isNotification = false;
+  bool isPersonalSetting = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+      bottomNavigationBar: const GNav(
+          backgroundColor: Colors.grey,
+          color: Colors.white,
+          activeColor: Colors.purple,
+          tabs: [
+            GButton(
+              icon: Icons.calendar_today,
+              text: 'Calendar',
+            ),
+            GButton(
+              icon: Icons.alarm,
+              text: 'Today Job',
+            ),
+            GButton(
+              icon: Icons.notifications,
+              text: 'Notifications',
+            ),
+            GButton(
+              icon: Icons.person,
+              text: 'Setting',
+            ),
+          ]),
+      // bottomNavigationBar: BottomAppBar(
+      //     notchMargin: 10,
+      //     shape: CircularNotchedRectangle(),
+      //     color: Colors.white,
+      //     child: IconTheme(
+      //       data: IconThemeData(color: Colors.grey),
+      //       child: Row(
+      //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //         children: [
+      //           IconButton(
+      //             isSelected: isCalendar,
+      //             tooltip: 'Calendar',
+      //             icon: Image.asset('assets/icons/ic_calendar.png'),
+      //             selectedIcon: Image.asset(
+      //               'assets/icons/ic_calendar.png',
+      //               color: Colors.purple,
+      //             ),
+      //             onPressed: () {
+      //               setState(() {
+      //                 isCalendar = true;
+      //                 isTodayjob = false;
+      //                 isNotification = false;
+      //                 isPersonalSetting = false;
+      //               });
+      //             },
+      //           ),
+      //           IconButton(
+      //             isSelected: isTodayjob,
+      //             tooltip: 'Today Job',
+      //             icon: Image.asset('assets/icons/ic_clock.png'),
+      //             selectedIcon: Image.asset(
+      //               'assets/icons/ic_clock.png',
+      //               color: Colors.purple,
+      //             ),
+      //             onPressed: () {
+      //               setState(() {
+      //                 isCalendar = false;
+      //                 isTodayjob = true;
+      //                 isNotification = false;
+      //                 isPersonalSetting = false;
+      //               });
+      //             },
+      //           ),
+      //           IconButton(
+      //             isSelected: isNotification,
+      //             tooltip: 'Notifications',
+      //             icon: Image.asset('assets/icons/ic_notification.png'),
+      //             selectedIcon: Image.asset(
+      //               'assets/icons/ic_notification.png',
+      //               color: Colors.purple,
+      //             ),
+      //             onPressed: () {
+      //               setState(() {
+      //                 isCalendar = false;
+      //                 isTodayjob = false;
+      //                 isNotification = true;
+      //                 isPersonalSetting = false;
+      //               });
+      //             },
+      //           ),
+      //           IconButton(
+      //             isSelected: isPersonalSetting,
+      //             tooltip: 'Personal Setting',
+      //             icon: Image.asset('assets/icons/ic_person.png'),
+      //             selectedIcon: Image.asset(
+      //               'assets/icons/ic_person.png',
+      //               color: Colors.purple,
+      //             ),
+      //             onPressed: () {
+      //               setState(() {
+      //                 isCalendar = false;
+      //                 isTodayjob = false;
+      //                 isNotification = false;
+      //                 isPersonalSetting = true;
+      //               });
+      //             },
+      //           ),
+      //         ],
+      //       ),
+      //     )),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -58,10 +156,22 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        backgroundColor: Colors.purple,
+        shape: CircleBorder(),
+        onPressed: () {
+          showModalBottomSheet<void>(
+            isScrollControlled: true,
+            context: context,
+            builder: (BuildContext context) => CreateEventWidget(),
+          );
+        },
         tooltip: 'Increment',
-        child: const Icon(Icons.place_sharp),
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
---
+++ lib/page/createEvent_page.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+
+class CreateEventWidget extends StatefulWidget {
+  const CreateEventWidget({super.key});
+
+  @override
+  State<CreateEventWidget> createState() => _CreateEventWidgetState();
+}
+
+final listCategory = <String>['Brainstorm', 'Design', 'Workout'];
+final listColor = <Color>[Colors.purple, Colors.green, Colors.blue];
+
+class _CreateEventWidgetState extends State<CreateEventWidget> {
+  bool light = false;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: 650,
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Add New Event',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                  borderSide: const BorderSide(width: 3, color: Colors.grey),
+                  borderRadius: BorderRadius.circular(15)),
+              hintText: 'Event name*',
+            ),
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            maxLines: 2,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                  borderSide: BorderSide(width: 3, color: Colors.grey),
+                  borderRadius: BorderRadius.circular(15)),
+              hintText: 'Type the note here...',
+            ),
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 3, color: Colors.grey),
+                    borderRadius: BorderRadius.circular(15)),
+                hintText: 'Date',
+                suffixIcon: Image.asset('assets/icons/ic_calendar.png')),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Expanded(
+                child: Container(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 3, color: Colors.grey),
+                            borderRadius: BorderRadius.circular(15)),
+                        hintText: 'Start time',
+                        suffixIcon: Image.asset('assets/icons/ic_clock.png')),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Container(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderSide:
+                                const BorderSide(width: 3, color: Colors.grey),
+                            borderRadius: BorderRadius.circular(15)),
+                        hintText: 'End time',
+                        suffixIcon: Image.asset('assets/icons/ic_clock.png')),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            children: [
+              const Text('Remind me'),
+              const Spacer(),
+              Switch(
+                  value: light,
+                  activeColor: Colors.purple,
+                  onChanged: (bool value) {
+                    setState(() {
+                      light = value;
+                    });
+                  })
+            ],
+          ),
+          const SizedBox(height: 10),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Select Category',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            height: 50,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: listCategory.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Row(
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 140,
+                        child: OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                            ),
+                            onPressed: () {},
+                            child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.circle_outlined,
+                                    size: 10,
+                                    color: listColor[index],
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    listCategory[index],
+                                    style: TextStyle(fontSize: 12),
+                                  ),
+                                ])),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      )
+                    ],
+                  );
+                }),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: InkWell(
+              child: Text(
+                "+ Add new",
+                style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.purple,
+                    decoration: TextDecoration.underline,
+                    decorationColor: Colors.purple),
+              ),
+              onTap: () {},
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 50,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  backgroundColor: Colors.purple),
+              onPressed: () {
+                Navigator.pop(context, 'ok');
+              },
+              child: const Text(
+                'Create Event',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+}
